b126b446998cf46a5468a5fd9e9259008ec01bea
bethmccusker <b.mccusker@lancaster.ac.uk>
2024-04-11 06:19:15 -0500

    added coord arrows

--- event_display_test.C
+++ event_display_test.C
@@ -42,16 +42,32 @@ void DrawTrack(TCanvas *c1, double* point1, double* point2, int colour)
   track->Draw();
 }
 
-void event_display(double *track_start,double *track_end){
+
+void event_display(double *track_start,double *track_end,double index){
+
   TCanvas *c1 = new TCanvas("c1","");
  
   double  rmin[3] {-200,-200,0}; 
   double  rmax[3] {-200,200,500};
 
   DrawCube(c1, rmin, rmax, kRed-6 );
-
  
-   DrawTrack(c1,track_start,track_end,kBlue-6);
+  DrawTrack(c1,track_start,track_end,kBlue-6);
+
+  double arrow_start_x = - 0.7;
+  double arrow_start_y = -0.8;
+  double arrow_length = 0.2; 
+  TLatex coord_y;
+  TLatex coord_z;
+  coord_y.DrawLatex(arrow_start_x-0.015,arrow_start_y+0.2+0.03, "y");
+  coord_z.DrawLatex(arrow_start_x+0.13 +0.02, arrow_start_y-0.02, "z");
+  TArrow *arrow_y = new TArrow(arrow_start_x, arrow_start_y, arrow_start_x, arrow_start_y+0.2, 0.01, "|>");
+  TArrow *arrow_z = new TArrow(arrow_start_x, arrow_start_y, arrow_start_x+0.13, arrow_start_y, 0.01, "|>");
+  arrow_y->SetLineWidth(2);
+  arrow_z->SetLineWidth(2);
+  arrow_y->Draw();
+  arrow_z->Draw();
+
 
   TView3D *view = (TView3D*) TView::CreateView(1);
 
@@ -65,14 +81,16 @@ void event_display(double *track_start,double *track_end){
 			    view->GetTnorm(),
 			    view->GetTback());
 
-  const TString SaveDir="/exp/sbnd/data/users/bethanym/wire_transparency/Selection_CRT";
-  c1->SaveAs(SaveDir + "/event_display.pdf");
- 
-  // delete c1;
+  const std::string SaveDir="/exp/sbnd/data/users/bethanym/wire_transparency/Selection_CRT";
+   std::string event = SaveDir + ("/event_display_" + std::to_string(index) + ".pdf");
+   c1->SaveAs(event.c_str());  
+ //   c1->SaveAs(SaveDir + "/event_display.pdf");
+
+   //  delete c1;
 }
 
 void event_display_test(){
-  double track_start[3] {-200,70,-100};                                                                                                                                                             
-  double track_end[3] {-200,-70,600};  
-  event_display( track_start,track_end);
+  // double track_start[3] {-200,70,-100};                                                                                                                                                             
+  //  double track_end[3] {-200,-70,600};  
+  //  event_display(double * track_start, double *track_end);
 }
